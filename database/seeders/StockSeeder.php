7e3875ce69ca7e6a2be9f860c05a401f7649cf03
Mladen Draskovic <draskovicmladen@gmail.com>
2024-04-26 21:46:15 +0200

    Add stock seeder

---
+++ database/seeders/StockSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Stock;
+use Illuminate\Database\Seeder;
+
+class StockSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if (Stock::query()->exists()) {
+            return;
+        }
+
+        $stocks = [
+            [
+                'name' => 'Alphabet Inc.',
+                'symbol' => 'GOOGL',
+            ],
+            [
+                'name' => 'Amazon.com Inc.',
+                'symbol' => 'AMZN',
+            ],
+            [
+                'name' => 'Apple Inc.',
+                'symbol' => 'AAPL',
+            ],
+            [
+                'name' => 'Meta Platforms Inc.',
+                'symbol' => 'META',
+            ],
+            [
+                'name' => 'Microsoft Corporation',
+                'symbol' => 'MSFT',
+            ],
+        ];
+
+        Stock::query()->insert($stocks);
+    }
+}
