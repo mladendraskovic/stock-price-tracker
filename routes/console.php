85ea2953df75cb66f7c6f90e602ae38b542f27a1
Mladen Draskovic <draskovicmladen@gmail.com>
2024-04-28 17:50:33 +0200

    Schedule command to fetch stock price data

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\FetchStockPriceData;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::command(FetchStockPriceData::class)
+    ->everyMinute()
+    ->withoutOverlapping();
